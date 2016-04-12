41f44475f3fe3c381a808622f79f3237a5d7fff6
Anders Sjobom <anders.sjobom@live.se>
2016-04-12 10:30:59 +0200

    Implementerat kartmodul inklusive GPS-zoom.

--- app/src/main/java/se/anderssjobom/weathertracker/MainActivity.java
+++ app/src/main/java/se/anderssjobom/weathertracker/MainActivity.java
@@ -1,13 +1,177 @@
 package se.anderssjobom.weathertracker;
 
+import android.Manifest;
+import android.app.Dialog;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+
+public class MainActivity extends AppCompatActivity
+        implements OnMapReadyCallback,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        ActivityCompat.OnRequestPermissionsResultCallback{
+
+    private static final int GPS_ZOOM_PERMISSION_CODE = 1; //Application specific request code to match with a result reported to onRequestPermissionsResult(int, String[], int[]).
+    GoogleMap mMap; //Kartreferens, initialiseras i initMap
+    private static final int ERROR_DIALOG_REQUEST = 9001;
+    private GoogleApiClient mLocationClient; //För GPS
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+
+        if (servicesOK()) {
+            setContentView(R.layout.activity_map);
+            //Initialisera kartan
+            initMap();
+            //Initialisera GPS
+            mLocationClient = new GoogleApiClient.Builder(this)
+                    .addApi(LocationServices.API)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .build();
+            mLocationClient.connect();
+        } else {
+            setContentView(R.layout.activity_main);
+        }
+    }
+
+    public boolean servicesOK() {
+        int isAvailable = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+
+        if (isAvailable == ConnectionResult.SUCCESS) {
+            return true;
+        } else if (GooglePlayServicesUtil.isUserRecoverableError(isAvailable)) {
+            Dialog dialog =
+                    GooglePlayServicesUtil.getErrorDialog(isAvailable, this, ERROR_DIALOG_REQUEST);
+            dialog.show();
+        } else {
+            Toast.makeText(this, "Can't connect to mapping service", Toast.LENGTH_SHORT).show();
+        }
+
+        return false;
+    }
+
+    public void setCurrentLocation() {
+        if(ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED ||
+                ActivityCompat.checkSelfPermission(
+                        this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+                            Manifest.permission.ACCESS_COARSE_LOCATION},
+                    GPS_ZOOM_PERMISSION_CODE);
+            return;
+        }
+        Location currentLocation = LocationServices.FusedLocationApi
+                .getLastLocation(mLocationClient);
+        LatLng latLng;
+        int zoomLevel;
+        //TODO - Ibland blir currentLocation null, kanske använda annan lösning?
+        if (currentLocation == null) {
+            latLng = new LatLng(
+                    currentLocation.getLatitude(),
+                    currentLocation.getLongitude()
+            );
+            zoomLevel = 12;
+        } else {
+            latLng = new LatLng(62.386504, 16.320447);
+            zoomLevel = 5;
+        }
+        CameraUpdate update = CameraUpdateFactory.newLatLngZoom(
+                latLng, zoomLevel
+        );
+        mMap.animateCamera(update);
+    }
+
+    @Override
+    public void onRequestPermissionsResult (int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case GPS_ZOOM_PERMISSION_CODE: {
+                if (permissions.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                        && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                    Log.d("PERMISSION", "GRANTED");
+                    setCurrentLocation();
+                }else{
+                    LatLng latLng = new LatLng(62.386504, 16.320447);
+                    CameraUpdate update = CameraUpdateFactory.newLatLngZoom(
+                            latLng, 5
+                    );
+                    mMap.animateCamera(update);
+                }
+            }
+        }
+    }
+
+    private void initMap(){
+        if (mMap == null){
+            MapFragment mapFragment =
+                    (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+            mapFragment.getMapAsync(this); //skapar kartan och anropar onMapReady när kartan är klar
+        }
+    }
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+    }
+
+    // De tre funktionerna nedan krävs för GPS
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        if(ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED ||
+                ActivityCompat.checkSelfPermission(
+                        this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+                            Manifest.permission.ACCESS_COARSE_LOCATION},
+                    GPS_ZOOM_PERMISSION_CODE);
+            return;
+        }
+        Location currentLocation = LocationServices.FusedLocationApi
+                .getLastLocation(mLocationClient);
+
+        LatLng latLng = new LatLng(
+                currentLocation.getLatitude(),
+                currentLocation.getLongitude()
+        );
+        CameraUpdate update = CameraUpdateFactory.newLatLngZoom(
+                latLng, 15
+        );
+        mMap.animateCamera(update);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
