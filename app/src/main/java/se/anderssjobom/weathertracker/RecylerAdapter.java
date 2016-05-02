fdb6c54ea09cafe050222dd4448e5252d5687a2e
Rickard <pickan@live.se>
2016-05-02 09:19:54 +0200

    asd

--- app/src/main/java/se/anderssjobom/weathertracker/RecylerAdapter.java
+++ app/src/main/java/se/anderssjobom/weathertracker/RecylerAdapter.java
@@ -1,13 +1,21 @@
 package se.anderssjobom.weathertracker;
 
+import android.content.Context;
+import android.os.Vibrator;
+import android.support.v7.app.ActionBar;
 import android.support.v7.widget.RecyclerView;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.PopupWindow;
 import android.widget.TextView;
-import android.widget.Toast;
 
+
+
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
 /**
@@ -15,22 +23,26 @@ import java.util.ArrayList;
  */
 public class RecylerAdapter extends RecyclerView.Adapter<RecylerAdapter.RecyclerViewHolder> {
 
-    private ArrayList<DataProvider> arrayList = new ArrayList<DataProvider>();
+    private static ArrayList<DataProvider> arrayList = new ArrayList<>();
+    public static boolean local_pressed = false;
+    public static PopupWindow popupWindow;
+    public static Button button1;
+    public static Button button2;
+    public static String ID_ACTIVITY_BTN;
 
 
 
-    public RecylerAdapter(ArrayList<DataProvider> dataProviders)
-    {
+    public RecylerAdapter(ArrayList<DataProvider> dataProviders) {
         this.arrayList = dataProviders;
     }
+
 
     @Override
     public RecyclerViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
 
         //Skapar en view som ska "inflate" dvs visa vår row_layout som definerar formen på våra element,
         //inuti vår recyclerview.
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_layout,parent,false);
-
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_layout, parent, false);
         RecyclerViewHolder recyclerViewHolder = new RecyclerViewHolder(view);
 
 
@@ -40,9 +52,8 @@ public class RecylerAdapter extends RecyclerView.Adapter<RecylerAdapter.Recycler
 
     //Kopplar ihop en posistion i vår RecyclerView till varje Viewholder-element.
     @Override
-    public void onBindViewHolder(final RecyclerViewHolder holder, final int position)
-    {
-       final DataProvider dataProvider = arrayList.get(position);
+    public void onBindViewHolder(final RecyclerViewHolder holder, final int position) {
+        final DataProvider dataProvider = arrayList.get(position);
         holder.Im_Activity.setImageResource(dataProvider.getImg_res());
         holder.Tx_Activity.setText(dataProvider.getA_name());
     }
@@ -57,24 +68,68 @@ public class RecylerAdapter extends RecyclerView.Adapter<RecylerAdapter.Recycler
         ImageView Im_Activity;
         TextView Tx_Activity;
 
-        public RecyclerViewHolder(View view)
+
+        public RecyclerViewHolder(View Gridview)
         {
-            super(view);
-            Tx_Activity = (TextView) view.findViewById(R.id.activity_name);
-            Im_Activity = (ImageView) view.findViewById(R.id.activity_image);
-            Im_Activity.setOnClickListener(this);
+            super(Gridview);
+            Gridview.setOnClickListener(this);
 
 
+            Tx_Activity = (TextView) Gridview.findViewById(R.id.activity_name);
+            Im_Activity = (ImageView) Gridview.findViewById(R.id.activity_image);
 
         }
+
 
         @Override
         public void onClick(View v) {
+            // vibrator för knapptryck
+            Vibrator vibrator = (Vibrator) v.getContext().getSystemService(Context.VIBRATOR_SERVICE);
+            vibrator.vibrate(20);
 
-            Toast.makeText(v.getContext(),"Du har valt " +Tx_Activity.getText().toString(),Toast.LENGTH_SHORT).show();
+            View popView = View.inflate(v.getContext(), R.layout.popup,null);
+            popupWindow = new PopupWindow(popView, ActionBar.LayoutParams.MATCH_PARENT, ActionBar.LayoutParams.WRAP_CONTENT,true);
+            popupWindow.setAnimationStyle(android.R.style.Animation_InputMethod);
+            popupWindow.showAtLocation(popView, Gravity.BOTTOM,0,0);
+            popView.setAlpha((float)0.8);
 
+            button1 = (Button)popView.findViewById(R.id.DateButton);
+            button2 = (Button)popView.findViewById(R.id.DateButton2);
+
+
+            //Lägg till eller ta bort denna kod om man vill ha startdatum på båda datumknappar istället för "hints" om start och slutdatum
+            someCode();
+
+
+            //om tryckt - ändra alpha
+            if (!local_pressed) {
+                Im_Activity.setImageAlpha(100);
+                Tx_Activity.setAlpha((float) 0.6);
+                local_pressed = true;
+            }
+
+            popupWindow.setOnDismissListener(new PopupWindow.OnDismissListener() {
+                @Override
+                public void onDismiss() {
+                    Im_Activity.setImageAlpha(255);
+                    Tx_Activity.setAlpha((float) 1);
+                    local_pressed = false;
+                    someCode();
+                }
+            });
 
 
         }
+
     }
+
+    public static void someCode(){
+        String df1 = new SimpleDateFormat("yyyy-MM-dd").format(MainActivity.buttText1.getTime());
+        String df2 = new SimpleDateFormat("yyyy-MM-dd").format(MainActivity.buttText2.getTime());
+        button1.setText(df1);
+        button2.setText(df2);
+    }
+
+
 }
+
